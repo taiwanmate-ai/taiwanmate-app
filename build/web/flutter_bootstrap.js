d3821198db852caa9bfcaa031816541b5e791c45
taiwanmate-ai <hailuongvan201199@gmail.com>
2026-06-10 22:41:37 +0800

    feat: thêm tool Du học Đài Loan A-Z với dữ liệu chính xác

--- build/web/flutter_bootstrap.js
+++ build/web/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"c416acfeb8126e097f758c664aaa3da929e27d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "490270051" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "4182819983" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
