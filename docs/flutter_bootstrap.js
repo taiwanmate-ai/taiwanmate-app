969641dd9fc3e4a3957f5e0acc00023b1ac68cb3
taiwanmate-ai <hailuongvan201199@gmail.com>
2026-06-12 20:27:15 +0800

    fix: correct package name in payment_service import

--- docs/flutter_bootstrap.js
+++ docs/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"c416acfeb8126e097f758c664aaa3da929e27d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2671071327" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "3128799327" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
