77dc0762220416ca04b3ab2242cb163c61d405dc
taiwanmate-ai <hailuongvan201199@gmail.com>
2026-06-12 20:21:41 +0800

    fix: dynamic checkout URL with user_id for webhook VIP update

--- docs/flutter_bootstrap.js
+++ docs/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"c416acfeb8126e097f758c664aaa3da929e27d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "842079963" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "2671071327" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
