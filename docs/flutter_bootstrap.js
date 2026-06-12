806b9432d564bc6de6ef6bff2e099d4b3cbebe1f
taiwanmate-ai <hailuongvan201199@gmail.com>
2026-06-13 00:26:19 +0800

    add legal pages footer link

--- docs/flutter_bootstrap.js
+++ docs/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"c416acfeb8126e097f758c664aaa3da929e27d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1516382224" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "1094394686" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
