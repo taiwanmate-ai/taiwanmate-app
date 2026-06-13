cc336cca5e5483b3a5b331197117880045644021
taiwanmate-ai <hailuongvan201199@gmail.com>
2026-06-13 10:22:34 +0800

    hide legal footer after app loads

--- docs/flutter_bootstrap.js
+++ docs/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"c416acfeb8126e097f758c664aaa3da929e27d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3206359420" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "1795986743" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
