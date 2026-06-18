b5914e06d682e12dca0d5e5fa43e5437adfa7463
taiwanmate-ai <hailuongvan201199@gmail.com>
2026-06-18 22:11:35 +0800

    Add chat streaming

--- docs/flutter_bootstrap.js
+++ docs/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"77e2e94772b6eb43759e34ed1ad7da4674e19c
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "4233359644" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "1294046888" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
