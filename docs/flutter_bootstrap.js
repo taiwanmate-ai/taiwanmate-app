68ff985302c83c197854fc6afd5de53bb7995a4d
taiwanmate-ai <hailuongvan201199@gmail.com>
2026-06-20 14:50:35 +0800

    fix: handle 403 quota exceeded in journey roleplay

--- docs/flutter_bootstrap.js
+++ docs/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"77e2e94772b6eb43759e34ed1ad7da4674e19c
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "831045627" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "2397864441" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
