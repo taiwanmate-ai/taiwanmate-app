2a06ed00eb1add95d669d25c1e1746ab07103aef
taiwanmate-ai <hailuongvan201199@gmail.com>
2026-06-27 23:34:12 +0800

    fix: khôi phục encoding tiếng Việt

--- docs/flutter_bootstrap.js
+++ docs/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"77e2e94772b6eb43759e34ed1ad7da4674e19c
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "551879352" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "1430150323" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
