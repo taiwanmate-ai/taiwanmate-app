55925708259500db4ae0f94bc4d571ad6d7008ef
taiwanmate-ai <hailuongvan201199@gmail.com>
2026-07-12 04:05:43 +0800

    feat: he thong luyen phat am thong minh - khong lap cau da thuoc (>=80 diem)

--- docs/flutter_bootstrap.js
+++ docs/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"77e2e94772b6eb43759e34ed1ad7da4674e19c
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2415504821" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "2756661704" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
