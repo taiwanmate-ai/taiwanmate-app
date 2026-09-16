512c0554d35f9181246a701bd70027b1e1b412cc
taiwanmate-ai <hailuongvan201199@gmail.com>
2026-09-16 22:00:08 +0800

    Build web release: fix live-chat hardcoded URLs and quota bugs

--- docs/flutter_bootstrap.js
+++ docs/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"77e2e94772b6eb43759e34ed1ad7da4674e19c
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1259083370" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "2159437413" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
